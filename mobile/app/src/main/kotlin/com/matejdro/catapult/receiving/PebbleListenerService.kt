febf8abd9cbea3f660bd1b53283b27b4d8221879
Matej Drobnič <matej@matejdro.com>
2025-12-31 12:23:11 +0100

    refactor(app): fix PebbleListenerService incremental compilation

--- mobile/app/src/main/kotlin/com/matejdro/catapult/receiving/PebbleListenerService.kt
+++ mobile/app/src/main/kotlin/com/matejdro/catapult/receiving/PebbleListenerService.kt
@@ -1,15 +1,10 @@
 package com.matejdro.catapult.receiving
 
-import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import com.matejdro.catapult.CatapultApplication
 import com.matejdro.catapult.bluetooth.WatchappConnectionsManager
-import com.matejdro.catapult.di.ServiceKey
-import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.ContributesIntoMap
 import dev.zacsweers.metro.Inject
-import dev.zacsweers.metro.binding
 import dispatch.core.DefaultCoroutineScope
 import io.rebble.pebblekit2.client.BasePebbleListenerService
 import io.rebble.pebblekit2.common.model.PebbleDictionary
@@ -17,9 +12,6 @@ import io.rebble.pebblekit2.common.model.ReceiveResult
 import io.rebble.pebblekit2.common.model.WatchIdentifier
 import java.util.UUID
 
-@ContributesIntoMap(AppScope::class, binding = binding<Service>())
-@ServiceKey(PebbleListenerService::class)
-@Inject
 class PebbleListenerService : BasePebbleListenerService() {
    @Inject
    @Volatile
