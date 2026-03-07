matejdro/microPebble

feat(common-navigation): improve predictive back animation

common-navigation/src/main/kotlin/com/matejdro/micropebble/navigation/animation/PredictiveBackFadeAnimationSpec.kt
@@ -1,0 +1,73 @@
+package com.matejdro.micropebble.navigation.animation
+
+import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.animation.core.AnimationVector
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.FloatAnimationSpec
+import androidx.compose.animation.core.FloatTweenSpec
+import androidx.compose.animation.core.TwoWayConverter
+import androidx.compose.animation.core.VectorizedFiniteAnimationSpec
+import androidx.compose.animation.core.VectorizedFloatAnimationSpec
+
+internal class PredictiveBackFadeAnimationSpec(
+   private val durationMillis: Int = AnimationConstants.DefaultDurationMillis,
+   private val easing: Easing = FastOutSlowInEasing,
+) : FiniteAnimationSpec<Float> {
+   override fun <V : AnimationVector> vectorize(converter: TwoWayConverter<Float, V>): VectorizedFiniteAnimationSpec<V> {
+      return VectorizedFloatAnimationSpec(PredictiveBackFadeFloatAnimationSpec(durationMillis, easing))
+   }
+}
+
+private class PredictiveBackFadeFloatAnimationSpec(
+   public val duration: Int = AnimationConstants.DefaultDurationMillis,
+   private val easing: Easing = FastOutSlowInEasing,
+) : FloatAnimationSpec {
+   private val tweenSpec = FloatTweenSpec(duration, easing = easing)
+   override fun getDurationNanos(
+      initialValue: Float,
+      targetValue: Float,
+      initialVelocity: Float,
+   ): Long {
+      return tweenSpec.getDurationNanos(initialValue, targetValue, initialVelocity)
+   }
+
+   @Suppress("MagicNumber") // Commented
+   override fun getValueFromNanos(
+      playTimeNanos: Long,
+      initialValue: Float,
+      targetValue: Float,
+      initialVelocity: Float,
+   ): Float {
+      // Animation spec that attempts to mimic Google's back preview guidelines as close as possible
+      // https://developer.android.com/design/ui/mobile/guides/patterns/predictive-back#back-preview
+
+      val position = tweenSpec.getValueFromNanos(
+         playTimeNanos = playTimeNanos,
+         initialValue = 0f,
+         targetValue = 1f,
+         initialVelocity = initialVelocity
+      )
+      return if (targetValue < 0.5f) {
+         // We are in "Fade Out" animation. Complete the fade at 35%
+         (1f - (position * 2.85f)).coerceAtLeast(0f)
+      } else {
+         // We are in "Fade In" animation. Start fading at 35%
+         if (position < 0.35f) {
+            0f
+         } else {
+            (position - 0.35f) * 1 / (1 - 0.35f)
+         }
+      }
+   }
+
+   override fun getVelocityFromNanos(
+      playTimeNanos: Long,
+      initialValue: Float,
+      targetValue: Float,
+      initialVelocity: Float,
+   ): Float {
+      return tweenSpec.getVelocityFromNanos(playTimeNanos, initialValue, targetValue, initialVelocity)
+   }
+}

common-navigation/src/main/kotlin/com/matejdro/micropebble/navigation/keys/base/BaseScreenKey.kt
@@ -9,24 +9,25 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.ui.graphics.TransformOrigin
+import com.matejdro.micropebble.navigation.animation.PredictiveBackFadeAnimationSpec
 import si.inova.kotlinova.navigation.screenkeys.ScreenKey
 
 abstract class BaseScreenKey : ScreenKey() {
    @Suppress("MagicNumber") // Magic numbers are the whole point of this function
    override fun backAnimation(scope: AnimatedContentTransitionScope<*>, backSwipeEdge: Int?): ContentTransform {
-      // Animation spec that attempts to mimic Google's back preview guidelines as close as possible
-      // https://developer.android.com/design/ui/mobile/guides/patterns/predictive-back#back-preview
-
-      // See https://issuetracker.google.com/issues/347047848 for feature request to be able
-      // to implement the guidelines fully
-
       val scaleTransformOrigin = when (backSwipeEdge) {
          BackEventCompat.EDGE_LEFT -> TransformOrigin(pivotFractionX = 1f, pivotFractionY = 0.5f)
          BackEventCompat.EDGE_RIGHT -> TransformOrigin(pivotFractionX = 0f, pivotFractionY = 0.5f)
          else -> TransformOrigin.Center
       }
 
-      return (fadeIn() + scaleIn(initialScale = 1.1f, transformOrigin = scaleTransformOrigin)) togetherWith
-         (fadeOut() + scaleOut(targetScale = 0.9f, transformOrigin = scaleTransformOrigin))
+      return (
+         fadeIn(PredictiveBackFadeAnimationSpec()) +
+            scaleIn(initialScale = 1.1f, transformOrigin = scaleTransformOrigin)
+         ) togetherWith
+         (
+            fadeOut(PredictiveBackFadeAnimationSpec()) +
+               scaleOut(targetScale = 0.9f, transformOrigin = scaleTransformOrigin)
+            )
    }
 }

matejdro/PebbleCatapult

mobile/common-navigation/src/main/kotlin/com/matejdro/catapult/navigation/animation/PredictiveBackFadeAnimationSpec.kt
@@ -1,0 +1,73 @@
+package com.matejdro.catapult.navigation.animation
+
+import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.animation.core.AnimationVector
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.FloatAnimationSpec
+import androidx.compose.animation.core.FloatTweenSpec
+import androidx.compose.animation.core.TwoWayConverter
+import androidx.compose.animation.core.VectorizedFiniteAnimationSpec
+import androidx.compose.animation.core.VectorizedFloatAnimationSpec
+
+internal class PredictiveBackFadeAnimationSpec(
+   private val durationMillis: Int = AnimationConstants.DefaultDurationMillis,
+   private val easing: Easing = FastOutSlowInEasing,
+) : FiniteAnimationSpec<Float> {
+   override fun <V : AnimationVector> vectorize(converter: TwoWayConverter<Float, V>): VectorizedFiniteAnimationSpec<V> {
+      return VectorizedFloatAnimationSpec(PredictiveBackFadeFloatAnimationSpec(durationMillis, easing))
+   }
+}
+
+private class PredictiveBackFadeFloatAnimationSpec(
+   public val duration: Int = AnimationConstants.DefaultDurationMillis,
+   private val easing: Easing = FastOutSlowInEasing,
+) : FloatAnimationSpec {
+   private val tweenSpec = FloatTweenSpec(duration, easing = easing)
+   override fun getDurationNanos(
+      initialValue: Float,
+      targetValue: Float,
+      initialVelocity: Float,
+   ): Long {
+      return tweenSpec.getDurationNanos(initialValue, targetValue, initialVelocity)
+   }
+
+   @Suppress("MagicNumber") // Commented
+   override fun getValueFromNanos(
+      playTimeNanos: Long,
+      initialValue: Float,
+      targetValue: Float,
+      initialVelocity: Float,
+   ): Float {
+      // Animation spec that attempts to mimic Google's back preview guidelines as close as possible
+      // https://developer.android.com/design/ui/mobile/guides/patterns/predictive-back#back-preview
+
+      val position = tweenSpec.getValueFromNanos(
+         playTimeNanos = playTimeNanos,
+         initialValue = 0f,
+         targetValue = 1f,
+         initialVelocity = initialVelocity
+      )
+      return if (targetValue < 0.5f) {
+         // We are in "Fade Out" animation. Complete the fade at 35%
+         (1f - (position * 2.85f)).coerceAtLeast(0f)
+      } else {
+         // We are in "Fade In" animation. Start fading at 35%
+         if (position < 0.35f) {
+            0f
+         } else {
+            (position - 0.35f) * 1 / (1 - 0.35f)
+         }
+      }
+   }
+
+   override fun getVelocityFromNanos(
+      playTimeNanos: Long,
+      initialValue: Float,
+      targetValue: Float,
+      initialVelocity: Float,
+   ): Float {
+      return tweenSpec.getVelocityFromNanos(playTimeNanos, initialValue, targetValue, initialVelocity)
+   }
+}

mobile/common-navigation/src/main/kotlin/com/matejdro/catapult/navigation/keys/base/BaseScreenKey.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.ui.graphics.TransformOrigin
+import com.matejdro.catapult.navigation.animation.PredictiveBackFadeAnimationSpec
 import si.inova.kotlinova.navigation.screenkeys.ScreenKey
 
 abstract class BaseScreenKey : ScreenKey() {
@@ -17,19 +18,19 @@ abstract class BaseScreenKey : ScreenKey() {
       scope: AnimatedContentTransitionScope<*>,
       backSwipeEdge: @BackEventCompat.SwipeEdge Int?,
    ): ContentTransform {
-      // Animation spec that attempts to mimic Google's back preview guidelines as close as possible
-      // https://developer.android.com/design/ui/mobile/guides/patterns/predictive-back#back-preview
-
-      // See https://issuetracker.google.com/issues/347047848 for feature request to be able
-      // to implement the guidelines fully
-
       val scaleTransformOrigin = when (backSwipeEdge) {
          BackEventCompat.EDGE_LEFT -> TransformOrigin(pivotFractionX = 1f, pivotFractionY = 0.5f)
          BackEventCompat.EDGE_RIGHT -> TransformOrigin(pivotFractionX = 0f, pivotFractionY = 0.5f)
          else -> TransformOrigin.Center
       }
 
-      return (fadeIn() + scaleIn(initialScale = 1.1f, transformOrigin = scaleTransformOrigin)) togetherWith
-         (fadeOut() + scaleOut(targetScale = 0.9f, transformOrigin = scaleTransformOrigin))
+      return (
+         fadeIn(PredictiveBackFadeAnimationSpec()) +
+            scaleIn(initialScale = 1.1f, transformOrigin = scaleTransformOrigin)
+         ) togetherWith
+         (
+            fadeOut(PredictiveBackFadeAnimationSpec()) +
+               scaleOut(targetScale = 0.9f, transformOrigin = scaleTransformOrigin)
+            )
    }
 }